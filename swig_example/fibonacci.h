311f09bdb424530466b01b3b96124e94bc67726a
IsaacRodgz <chack.rb@gmail.com>
2019-10-03 22:08:47 -0500

    Test example swig c++ python

---
+++ swig_example/fibonacci.h
@@ -0,0 +1,7 @@
+/* file fibonacci.h */
+#ifndef FIBONACCI_H
+#define FIBONACCI_H
+long fibonacci_recursion( long n);
+long fibonacci_iteration( long n, long nmax);
+long fibonacci_full_iteration( long n, long nmax);
+#endif
