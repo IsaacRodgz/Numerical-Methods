d2c2e920aa20c833b5968167dfe7ea12d8fe1341
isaacRodgz <chack.rb@gmail.com>
2019-10-15 16:24:40 -0500

    [ADD] newton interpolation files

---
+++ interpolation/interpolation1/interp.c
@@ -0,0 +1,16 @@
+/*
+Author: Isaac Rodríguez Bribiesca
+Date: 2019-10-15
+*/
+
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+#include "interp.h"
+#define TRUE 1
+#define FALSE 0
+
+void newton_interp(){
+
+    
+}
---
+++ interpolation/interpolation1/interp.h
@@ -0,0 +1,11 @@
+/*
+Author: Isaac Rodríguez Bribiesca
+Date: 2019-10-15
+*/
+
+#ifndef _interp_h
+#define _interp_h
+
+#include "interp.h"
+
+#endif
