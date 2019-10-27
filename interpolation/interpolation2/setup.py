68ed1c8525b1536e47b0511c8bd6b864d8b54f92
IsaacRodgz <chack.rb@gmail.com>
2019-10-26 20:59:16 -0500

    [ADD] Jupyter notebook

--- interpolation/interpolation2/setup.py
+++ interpolation/interpolation2/setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup, Extension
 import numpy
 import os
 
-name = "interp1"      # interpolation1 module
+name = "interp2"      # interpolation1 module
 version = "1.0"
 
 setup(name=name, version=version,
-      ext_modules=[Extension(name='_interp1', 
-             sources=["interp.i", "src/interp.c"],
+      ext_modules=[Extension(name='_interp2',
+             sources=["interp.i", "src/interp2.c"],
              include_dirs=['src', numpy.get_include(), '.'])
     ])
