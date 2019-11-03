c494b9af03def90555b0243bf8f28ac3d807d478
IsaacRodgz <chack.rb@gmail.com>
2019-11-02 20:43:06 -0600

    [ADD] run options

--- integration/src/test.c
+++ integration/src/test.c
@@ -1,23 +1,48 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include "integration.h"
 #define PI 3.14159266
 
 int main(int argc, char const *argv[]) {
 
-    //printf("\nIntegral sin(x): %lf\n", trapezoidal_rule(sin_x, 0, 4*PI, 20));
-    //printf("\nIntegral sin^2(x): %lf\n\n", trapezoidal_rule(sin_sqr_x, 0, PI, 6));
+    if (argc == 1) {
 
-    //printf("\nIntegral sin(x): %lf\n", simpson3_rule(sin_x, 0, 4*PI, 20));
-    //printf("\nIntegral sin^2(x): %lf\n\n", simpson3_rule(sin_sqr_x, 0, PI, 6));
+        printf("\nOne missing parameter: integral_rule_number\n");
 
-    //printf("\nIntegral sin(x): %lf\n", simpson8_rule(sin_x, 0, 4*PI, 20));
-    //printf("\nIntegral sin^2(x): %lf\n\n", simpson8_rule(sin_sqr_x, 0, PI, 6));
+        return 1;
+    }
 
-    //printf("\nIntegral sin(x): %lf\n", boole_rule(sin_x, 0, 4*PI, 20));
-    //printf("\nIntegral sin^2(x): %lf\n\n", boole_rule(sin_sqr_x, 0, PI, 6));
+    switch (atoi(argv[1])) {
+        case 1:
+            printf("\nIntegral of sin(x + 0.5) in interval [0, 4*pi] with 20 points: %e\n", trapezoidal_rule(sin_x, 0, 4*PI, 20));
+            printf("\nIntegral of sin(x + 0.5) in interval [0, 4*pi] with 100 points: %e\n", trapezoidal_rule(sin_x, 0, 4*PI, 100));
+            printf("\nIntegral of sin(x + 0.5) in interval [0, 4*pi] with 1000 points: %e\n\n", trapezoidal_rule(sin_x, 0, 4*PI, 1000));
+            break;
 
-    printf("\nIntegral sin(x): %lf\n", weddle_rule(sin_x, 0, 4*PI, 20));
-    printf("\nIntegral sin^2(x): %lf\n\n", weddle_rule(sin_sqr_x, 0, PI, 6));
+        case 2:
+            printf("\nIntegral of sin(x + 0.5) in interval [0, 4*pi] with 20 points: %e\n", simpson3_rule(sin_x, 0, 4*PI, 20));
+            printf("\nIntegral of sin(x + 0.5) in interval [0, 4*pi] with 100 points: %e\n", simpson3_rule(sin_x, 0, 4*PI, 100));
+            printf("\nIntegral of sin(x + 0.5) in interval [0, 4*pi] with 1000 points: %e\n\n", simpson3_rule(sin_x, 0, 4*PI, 1000));
+            break;
+
+        case 3:
+            printf("\nIntegral of sin(x + 0.5) in interval [0, 4*pi] with 20 points: %e\n", simpson8_rule(sin_x, 0, 4*PI, 20));
+            printf("\nIntegral of sin(x + 0.5) in interval [0, 4*pi] with 100 points: %e\n", simpson8_rule(sin_x, 0, 4*PI, 100));
+            printf("\nIntegral of sin(x + 0.5) in interval [0, 4*pi] with 1000 points: %e\n\n", simpson8_rule(sin_x, 0, 4*PI, 1000));
+            break;
+
+        case 4:
+            printf("\nIntegral of sin(x + 0.5) in interval [0, 4*pi] with 20 points: %e\n", boole_rule(sin_x, 0, 4*PI, 20));
+            printf("\nIntegral of sin(x + 0.5) in interval [0, 4*pi] with 100 points: %e\n", boole_rule(sin_x, 0, 4*PI, 100));
+            printf("\nIntegral of sin(x + 0.5) in interval [0, 4*pi] with 1000 points: %e\n\n", boole_rule(sin_x, 0, 4*PI, 1000));
+            break;
+
+        case 6:
+            printf("\nIntegral of sin(x + 0.5) in interval [0, 4*pi] with 20 points: %e\n", weddle_rule(sin_x, 0, 4*PI, 20));
+            printf("\nIntegral of sin(x + 0.5) in interval [0, 4*pi] with 100 points: %e\n", weddle_rule(sin_x, 0, 4*PI, 100));
+            printf("\nIntegral of sin(x + 0.5) in interval [0, 4*pi] with 1000 points: %e\n\n", weddle_rule(sin_x, 0, 4*PI, 1000));
+            break;
+    }
 
     return 0;
 }
