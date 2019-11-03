4c5b53248d74bb4145ee92460beda36a71cfcd5c
IsaacRodgz <chack.rb@gmail.com>
2019-11-03 12:18:21 -0600

    [MODIFY] test file

---
+++ integration/src/Trapecio/test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "../integration.h"
+#define PI 3.14159266
+
+int main(int argc, char const *argv[]) {
+
+    printf("\nIntegral of sin(x + 0.5) in interval [0, 4*pi] with 20 points: %e\n", trapezoidal_rule(sin_x, 0, 4*PI, 20));
+    printf("\nIntegral of sin(x + 0.5) in interval [0, 4*pi] with 100 points: %e\n", trapezoidal_rule(sin_x, 0, 4*PI, 100));
+    printf("\nIntegral of sin(x + 0.5) in interval [0, 4*pi] with 1000 points: %e\n\n", trapezoidal_rule(sin_x, 0, 4*PI, 1000));
+
+    return 0;
+}
